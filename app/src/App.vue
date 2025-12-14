a924b38d8fb470bf7eaa1680edd4f2a839880d11
4jo <4jo@example.com>
2025-12-14 10:51:30 +0900

    WIP：mainページを実装

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import SaraButton from './components/SaraButton.vue'
+import { MENU_PRICE_115, MENU_PRICE_120, MENU_PRICE_130, MENU_PRICE_150 } from './const/menu_price'
+
+const minPrice = ref<string | undefined>(undefined)
+const dishPrices = ref<number[] | undefined>(undefined)
+
+watch(minPrice, (selectedPrice) => {
+  switch (selectedPrice){
+    case "115":
+      dishPrices.value = MENU_PRICE_115
+      break
+    case "120":
+      dishPrices.value = MENU_PRICE_120
+      break
+    case "130":
+      dishPrices.value = MENU_PRICE_130
+      break
+    case "150":
+      dishPrices.value = MENU_PRICE_150
+      break
+    default:
+      console.error(
+        `
+        [エラー]
+        入力された選択肢が想定している値と一致しません。
+        data: ${selectedPrice}
+        data_type: ${typeof selectedPrice}
+        `
+      )
+      break
+  }
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <select v-model="minPrice" placeholder="1皿の値段を選択してください">
+      <option disabled value="">1皿の値段を選択してください</option>
+      <option>115</option>
+      <option>120</option>
+      <option>130</option>
+      <option>150</option>
+    </select>
+  </div>
+
+  <div v-for="price in dishPrices">
+    <p>{{ price }}</p>
+  </div>
+  
+  <div v-if="minPrice">
+    <p>任意入力</p>
+  </div>
 </template>
 
 <style scoped></style>
