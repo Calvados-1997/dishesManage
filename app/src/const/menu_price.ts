84f273f0716dd1d149894674c1a7fa11d2c4a6d1
Jo Lily <lily@example.com>
2025-12-14 20:03:07 +0900

    定数定義を追加

--- app/src/const/menu_price.ts
+++ app/src/const/menu_price.ts
@@ -1,9 +1,9 @@
 export const MENU_PRICE_115: number[] = [115, 120, 130, 140, 150, 160, 180, 200, 250, 270, 300, 380]
 
-export const MENU_PRICE_120: number[] = []
+export const MENU_PRICE_120: number[] = [120, 130, 150, 170, 190, 210, 240, 270, 280, 390]
 
-export const MENU_PRICE_130: number[] = []
+export const MENU_PRICE_130: number[] = [130, 140, 160, 180, 200, 220, 250, 280, 290, 420]
 
-export const MENU_PRICE_150: number[] = []
+export const MENU_PRICE_150: number[] = [150, 170, 190, 210, 230, 260, 300, 430]
 
 export const MENU_PRICE_HOKKAIDO: number[] = []
