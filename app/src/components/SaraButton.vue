4d2d3cf9583f3e44199b44aff34f8b7663e6192f
4jo <4jo@example.com>
2025-12-14 10:51:06 +0900

    ボタンコンポーネントを追加

---
+++ app/src/components/SaraButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    const props = defineProps({
+        title:{
+            type: String,
+            default: "タイトル"
+        },
+        fontSize:{
+            type: String,
+            default: "10px"
+        },
+        color:{
+            type: String,
+            default: "black"
+        }
+    })
+
+    const styleObject = {
+        'font-size': props.fontSize,
+        'color': props.color
+    }
+</script>
+
+<template>
+    <button class="btn" type="button" :style="styleObject">{{ props.title }}</button>    
+</template>
+
+<style lang="css">
+    .btn{
+        padding: 5px 20px;
+    }
+</style>
