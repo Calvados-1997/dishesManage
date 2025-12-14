e7d2370e12b2b9b306b2f79e1b4c1edc25c0bdc1
4jo <4jo@example.com>
2025-12-14 10:50:22 +0900

    WIP：各皿の値段を定数化

---
+++ app/src/const/menu_price.ts
@@ -0,0 +1,30 @@
+export const MENU_PRICE_115: number[] = [
+    115,
+    120,
+    130,
+    140,
+    150,
+    160,
+    180,
+    200,
+    250,
+    270,
+    300,
+    380,
+]
+
+export const MENU_PRICE_120: number[] = [
+
+]
+
+export const MENU_PRICE_130: number[] = [
+
+]
+
+export const MENU_PRICE_150: number[] = [
+
+]
+
+export const MENU_PRICE_HOKKAIDO: number[] = [
+
+]
