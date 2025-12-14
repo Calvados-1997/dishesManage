148131f9194eaedce48cce9b46b24516282daec5
Jo Lily <lily@example.com>
2025-12-14 19:17:07 +0900

    input用コンポーネントを追加

---
+++ app/src/components/DishInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  inputType: {
+    type: String,
+    default: 'text',
+  },
+  placeholder: {
+    type: String,
+    defautl: '',
+  },
+})
+
+const input = defineModel('input')
+</script>
+
+<template>
+  <input
+    class="border"
+    :type="props.inputType"
+    :placeholder="props.placeholder"
+    v-model.trim="input"
+  />
+</template>
+
+<style lang="css"></style>
