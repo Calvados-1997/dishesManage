817b2c4a1e9b2c93ec2b9f1e104078f5a614bb85
Jo Lily <lily@example.com>
2026-08-02 22:20:49 +0900

    各フォルダの準備

---
+++ app/src/pages/DishCountPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>TODO: お皿カウンターメインページ</div>
+</template>
+
+<style scoped></style>
---
+++ app/src/pages/HomePage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>TODO: ホームページ</div>
+</template>
+
+<style scoped></style>
