b18da7ecc1e7e82caa1be9244acf6c0100d4a0be
Jo Lily <lily@example.com>
2026-08-03 00:22:23 +0900

    定数修正に伴う修正

--- app/src/pages/HomePage.vue
+++ app/src/pages/HomePage.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 import router from '@/router'
+import type { StandardPrice } from '@/types/price'
 import { ref } from 'vue'
-const selected = ref<string>('')
-const options = [
-  {text: '115', value: '115'},
-  {text: '120', value: '120'},
-  {text: '130', value: '130'},
-  {text: '150', value: '150'},
-]
+
+const selected = ref<StandardPrice | ''>('')
+const options: StandardPrice[] = ['115', '120', '130', '150']
 
 const toDishCountPage = () => {
   router.push({name: 'dish-count', params: { 'stdPrice': selected.value }})
@@ -19,8 +16,8 @@ const toDishCountPage = () => {
       <p>1皿の値段を選択してください</p>
       <div class="price-select-area flex">
         <select @change="toDishCountPage()" class="border" v-model="selected" placeholder="1皿の値段を選択してください">
-          <option v-for="option in options" :value="option.value" :key="option.text">
-            {{ option.text }}
+          <option v-for="option in options" :value="option" :key="option">
+            {{ option }}
           </option>
         </select>
       </div>
@@ -30,4 +27,5 @@ const toDishCountPage = () => {
 <style scoped>
 .price-select-area > select option {
   font-size: 17px;
-}</style>
+}
+</style>
