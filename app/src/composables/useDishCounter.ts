511c9cd01254037cd554b72054e131690fa71c94
Jo Lily <lily@example.com>
2026-08-15 13:25:22 +0900

    refactor dish-count composable

--- app/src/composables/useDishCounter.ts
+++ app/src/composables/useDishCounter.ts
@@ -1,84 +1,121 @@
 import { reactive, ref } from "vue"
 
-// お皿枚数カウント処理
+/**
+ * Dish-counter composable
+ */
 export const useDishCounter = () => {
     const dishCountMap = reactive(new Map<number, number>())
     const totalPrice = ref<number>(0)
     const totalDishCount = ref<number>(0)
 
-    /** 指定した価格リストでカウントマップを0初期化する */
-    const initCountMap = (prices: number[]) => {
+    /**
+     * init dish-count counter.
+     * 
+     * reset state, set dish-counter map from arg.
+     * @param prices array of number
+     */
+    const init = (prices: number[]) => {
+        dishCountMap.clear()
+        totalPrice.value = 0
+        totalDishCount.value = 0
+
+        // initialize key-pair map with zero count
         for (const p of prices) {
             dishCountMap.set(p, 0)
         }
     }
-    /** 指定価格のカウントを1増やす */
-    const addDishCount = (price: number) => {
-        const prevCnt = dishCountMap.get(price) || 0
-        dishCountMap.set(price, prevCnt+1)
+
+    const _addDishCountMap = (price: number) => {
+        const prevCnt = dishCountMap.get(price) ?? 0
+        dishCountMap.set(price, prevCnt + 1)
     }
-    /** 指定価格のカウントを1減らす（0未満にはならない） */
-    const decreaseDishCount = (price: number) => {
+    const _addTotalDishCount = () => {
+        totalDishCount.value++
+    }
+    const _addTotalPrice = (price: number) => {
+        totalPrice.value += price
+    }
+    /**
+     * add dish count, totalDishCount, totalPrice
+     * @param price number
+     */
+    const addCount = (price: number) => {
+        _addDishCountMap(price)
+        _addTotalDishCount()
+        _addTotalPrice(price)
+    }
+
+    const _exsistPrice = (price: number) => {
+        return dishCountMap.has(price)
+    }
+    /**
+     * add custom price dish count.
+     * 
+     * return true if added successfully.
+     * 
+     * return false if price exsist already.
+     * 
+     * @param price number
+     * @returns boolean
+     */
+    const addCustomPriceCount = (price: number) => {
+        const alreadyExsist = _exsistPrice(price)
+        if(alreadyExsist) return false
+
+        addCount(price)
+        return true
+    }
+
+    const _decreaseDishCount = (price: number) => {
         const currCnt = dishCountMap.get(price) || 0
         if(currCnt <= 0) return
 
         dishCountMap.set(price, currCnt-1)
     }
-    /** 総枚数を1増やす */
-    const addTotalDishCount = () => {
-        totalDishCount.value++
-    }
-    /** 総枚数を1減らす */
-    const decreaseTotalDishCount = () => {
+    const _decreaseTotalDishCount = () => {
         totalDishCount.value--
     }
-    /** 総枚数を0にリセットする */
-    const resetTotalDishCount = () => {
-        totalDishCount.value = 0
+    const _decreaseTotalPrice = (price: number) => {
+        totalPrice.value -= price
     }
-    /** 全価格のカウントを0にリセットする */
-    const resetDishCount = () => {
+    /**
+     * decrease count with specified price.
+     * @param price number
+     */
+    const decreaseCount = (price: number) => {
+        _decreaseDishCount(price)
+        _decreaseTotalDishCount()
+        _decreaseTotalPrice(price)
+    }
+
+    const _resetDishCountMap = () => {
         for (const key of dishCountMap.keys()){
             dishCountMap.set(key, 0)
         }
     }
-    /** 合計金額に加算する */
-    const addTotal = (price: number) => {
-        totalPrice.value += price
+    const _resetTotalDishCount = () => {
+        totalDishCount.value = 0
     }
-    /** 合計金額から減算する */
-    const decreaseTotal = (price: number) => {
-        totalPrice.value -= price
-    }
-    /** 合計金額を0にリセットする */
-    const resetTotal = () => {
+    const _resetTotalPrice = () => {
         totalPrice.value = 0
     }
-    /** 価格マップに指定された値段が存在するかチェックする */
-    const exsistPrice = (price: number) => {
-        return dishCountMap.has(price)
-    }
-    /** カスタム価格をマップに追加する */
-    const addCustomPriceCount = (price: number, initCount: number = 1) => {
-        dishCountMap.set(price, initCount)
-        return true
+    /**
+     * reset all count.
+     */
+    const resetAllCount = () => {
+        _resetDishCountMap()
+        _resetTotalDishCount()
+        _resetTotalPrice()
     }
 
     return { 
         dishCountMap,
         totalPrice,
         totalDishCount,
-        initCountMap, 
-        addDishCount, 
-        decreaseDishCount,
-        addTotal, 
-        decreaseTotal,
-        addTotalDishCount, 
-        decreaseTotalDishCount,
-        resetTotalDishCount,
-        resetDishCount,
-        resetTotal,
-        exsistPrice,
-        addCustomPriceCount
+        init,
+        addCount,
+        addCustomPriceCount,
+        decreaseCount,
+        resetAllCount,
     }
 }
