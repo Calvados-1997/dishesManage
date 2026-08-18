74e183bf80722cfcdb4992401ec6e38ae0ae75d9
Jo Lily <lily@example.com>
2026-08-19 01:04:51 +0900

    fix

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import DishCountPage from '@/pages/DishCountPage.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
